7a3b466c0b68cade014ae1e5c500645c5d57be33
Jhun Norman <alonzojhunnorman@gmail.com>
2023-06-02 08:27:31 +0800

    ticketing scratch

---
+++ database/migrations/2023_06_01_234001_create_tickets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->integer('status_id');
+            $table->integer('assigned_to');
+            $table->string('priority')->comment('low, medium, high, urgent');
+            $table->dateTime('closed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
